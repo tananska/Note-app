ff52d75b6ac1cdc91a7f138fdc24fabcf5d1422c
Iva <ivatananska@abv.bg>
2021-08-24 00:59:34 +0300

    redux and app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import NoteList from "./components/NotesList";
+import Modal from "./components/Modal";
+import store from "./redux/store";
+import {Provider} from 'react-redux';
 
-function App() {
+
+
+function App(){
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store= {store}>
+      <div className='container'>
+        <Header />
+        <Modal />
+        <NoteList />
+        <Footer />
+      </div>
+    </Provider>
   );
-}
+};
 
 export default App;
